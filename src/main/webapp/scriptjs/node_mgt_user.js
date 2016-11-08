a796bbf6e0be19174c326b8749ae5d5061c0ca7f
wuyanli <278925389@qq.com>
2016-11-09 00:54:12 +0800

    添加剩余页面国际化所需js

---
+++ src/main/webapp/scriptjs/node_mgt_user.js
@@ -0,0 +1,227 @@
+function AutoCheckLang(){ //检查缓存中之前所设置的语言
+    if(localStorage.langclose==1){
+        change_ch();
+    }
+    else if(localStorage.langclose==2){
+        change_en();
+    }
+    else{
+        localStorage.langclose=1;
+    }
+
+}
+function change_en(){//变为英文
+
+    $("#Dtask").html("TASK MANAGEMENT");
+    $("#Skins_id").html("skins");
+    $("#langli").html("language");
+    $("#logout_id").html("log out");
+    $("#CPCEP_id").html("node list");
+    $("#user_mangid").html("USER MANAGEMENT");
+    $("#task_mangid").html("TASK MANAGEMENT");
+    $("#log_mangid").html("LOG MANAGEMENT");
+    $("#home_id").html("Home");
+    $("#number_id").html("number");
+    $("#node_id").html("nodeID");
+    $("#node_state").html("node state");
+    $("#node_type").html("node type");
+    $("#node_name").html("node name");
+    $("#user_name").html("user name");
+    $("#task_number").html("Number of tasks being performed");
+    $("#his_task").html("Historical task number");
+    $("#cpu").html("Cup occupancy rate");
+    $("#ram").html("Memory usage");
+    $("#net").html("Bandwidth occupancy rate");
+    $("#operate").html("operate");
+    $("#his_number").html("number");
+    $("#task_name").html("task name");
+    $("#task_type").html("task type");
+    $("#time_info").html("time information");
+    $("#close_button").html("close");
+    $("#task_number1").html("number");
+    $("#task_name1").html("task name");
+    $("#task_type1").html("task type");
+    $("#time_info1").html("time information");
+    $("#resource_info").html("resource information");
+    $("#button_close_id").html("close");
+    $("#his_text").html("history task");
+    $("#sureText_id").html("6 tasks are being performed to determine the closure of the node？");
+    $("#sure").html("sure");
+    $("#cancel").html("cancel");
+    $("#back").html("back");
+    $("#node_list").html("node list");
+    $("#execute_time").html("execute time");
+    $("#start_time").html("start time");
+    $("#end_time").html("end time");
+    $("#sure_time").html("sure");
+    $("#time_text").html("time information");
+    $("#tips").html("tips");
+    $("#cpu_info").html("cpu information");
+    $("#cpu_sure").html("sure");
+    $("#datatableForNode").dataTable().fnDestroy();
+    $("#datatableForTask").dataTable().fnDestroy();
+    var table=$('#datatableForNode').dataTable({
+        "sPaginationType": "bs_full",
+        "sPaginate": false,
+        sDom: "<'row'<'dataTables_header clearfix'<'col-md-4'l><'col-md-8'Tf>r>>t<'row'<'dataTables_footer clearfix'<'col-md-6'i><'col-md-6'p>>>",
+        select:true,
+        oTableTools: {
+            aButtons: [ {
+                "sExtends": "select",
+                "sButtonText": "删除" ,
+                //"id":"deletebutton",
+            },"copy",  "csv", "pdf" ],
+            sSwfPath: "js/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
+        }
+
+    });
+    $('#datatableForTask').dataTable({
+        "sPaginationType": "bs_full",
+        "sPaginate": false,
+        sDom: "<'row'<'dataTables_header clearfix'<'col-md-4'l><'col-md-8'Tf>r>>t<'row'<'dataTables_footer clearfix'<'col-md-6'i><'col-md-6'p>>>",
+        select:true,
+        oTableTools: {
+            aButtons: [ "copy",  "csv", "pdf" ],
+            sSwfPath: "js/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
+        }
+
+    });
+    App.setPage("index");  //Set current page
+}
+function change_ch(){//变为中文
+    $("#Dtask").html("任务管理");
+    $("#Skins_id").html("皮肤");
+    $("#logout_id").html("注销");
+    $("#langli").html("更换语言");
+    $("#CPCEP_id").html("节点列表");
+    $("#user_mangid").html("用户管理");
+    $("#task_mangid").html("任务管理");
+    $("#log_mangid").html("日志管理");
+
+    $("#home_id").html("主页");
+    $("#number_id").html("编号");
+    $("#node_id").html("节点ID");
+    $("#node_state").html("节点状态");
+    $("#node_type").html("节点类型");
+    $("#node_name").html("节点名称");
+    $("#user_name").html("用户名");
+    $("#task_number").html("正在执行的任务数");
+    $("#his_task").html("历史执行任务数");
+    $("#cpu").html("cup占用率");
+    $("#ram").html("内存使用率");
+    $("#net").html("带宽占用率");
+    $("#operate").html("操作");
+    $("#his_number").html("编号");
+    $("#task_name").html("任务名称");
+    $("#task_type").html("任务类型");
+    $("#time_info").html("时间信息");
+    $("#close_button").html("关闭");
+    $("#task_number1").html("编号");
+    $("#task_name1").html("任务名称");
+    $("#task_type1").html("任务类型");
+    $("#time_info1").html("时间信息");
+    $("#resource_info").html("资源信息");
+     $("#button_close_id").html("关闭");
+     $("#his_text").html("历史任务");
+     $("#sureText_id").html("正在执行6个任务，确定关闭该节点？");
+     $("#sure").html("确定");
+     $("#cancel").html("取消");
+     $("#back").html("返回");
+     $("#node_list").html("任务列表");
+     $("#sure_time").html("确认");
+     $("#time_text").html("时间信息");
+     $("#tips").html("提示");
+     $("#cpu_info").html("cpu 动态信息");
+     $("#cpu_sure").html("确定");
+    $("#datatableForNode").dataTable().fnDestroy();
+    $("#datatableForTask").dataTable().fnDestroy();
+    var table=$('#datatableForNode').dataTable({
+        "sPaginationType": "bs_full",
+        "sPaginate": false,
+        sDom: "<'row'<'dataTables_header clearfix'<'col-md-4'l><'col-md-8'Tf>r>>t<'row'<'dataTables_footer clearfix'<'col-md-6'i><'col-md-6'p>>>",
+        select:true,
+        oTableTools: {
+            aButtons: [ {
+                "sExtends": "select",
+                "sButtonText": "删除" ,
+                //"id":"deletebutton",
+            },"copy",  "csv", "pdf" ],
+            sSwfPath: "js/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
+        },
+        "oLanguage": {//国际语言转化
+            "sLengthMenu": "显示 _MENU_ 记录",
+            "sZeroRecords": "对不起，查询不到任何相关数据",
+            "sEmptyTable": "未有相关数据",
+            "sLoadingRecords": "正在加载数据-请等待...",
+            "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_ 条记录。",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoEmpty":"当前显示 0 到 0 条，共 0 条记录。",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            }
+        }
+
+    });
+
+    App.setPage("index");
+}
+
+jQuery(document).ready(function() {	//首先渲染
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/node/getAllNode",//接口名字
+        dataType: "json",
+        success: function (data) {
+            var stringfortrlist = "";
+            for (var i = 0; i < data.data.length; i++) {
+                var idforlog=i+1;
+                var stringfortr ="<tr class=\"gradeX\">"+
+                    "<td ><input type=\"checkbox\" name=\"checkList\"></td>"+
+                    "<td>"+idforlog+"</td>"+
+                    "<td>"+data.data[i].nodeId+"</td>"+
+                    "<td class=\"center\">"+data.data[i].type+"</td>"+
+                    "<td class=\"center\">"+data.data[i].nodeName+"</td>"+
+                    "<td class=\"center\">"+data.data[i].nodeName+"</td>"+
+                    "<td class=\"center hidden-xs\">"+data.data[i].status+"</td>"+
+                    "<td class=\"hidden-xs\"><a onclick=\"changeToTaskView()\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].taskAmount+"</a></td>"+
+                    "<td class=\"center\"><a href=\"#table-modal-his\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].historyTaskAmount+"</a></td>"+
+                    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">38%</a></td>"+
+                    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">56%</a></td>"+
+                    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">28%</a></td>"+
+                    "<td class=\"center hidden-xs\"><a href=\"#table-modal-closeNode\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">关闭</a></td>"+
+                    "</tr>";
+                stringfortrlist = stringfortrlist + stringfortr;
+            }
+            $("#datatableForNode").dataTable().fnDestroy();
+            $('#tbodyfornodelist').html(stringfortrlist);
+            AutoCheckLang();
+        }
+    });
+
+    App.setPage("index");  //Set current page，这俩破玩意竟然和换肤有关
+    App.init(); //Initialise plugins and elements
+
+    $("#langli").click(function(){//点击选择更换语言
+        if(localStorage.lang){ //缓存 1代表中文，2代表英文
+            if(localStorage.lang==1){
+                localStorage.lang=2;
+                localStorage.langclose=2;
+                change_en();
+            }
+            else if(localStorage.lang==2){
+                localStorage.lang=1;
+                localStorage.langclose=1;
+                change_ch();
+            }
+        }
+        else{
+            localStorage.lang=2;
+            localStorage.langclose=2;
+            change_en();
+
+        }
+    });
+});
