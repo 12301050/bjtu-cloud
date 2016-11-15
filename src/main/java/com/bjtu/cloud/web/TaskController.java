7d99ab204b61acfdcbc24a4c21de120640098eeb
Kafukaaa <452369538@qq.com>
2016-11-16 00:25:22 +0800

    add file upload in api/task/create

--- src/main/java/com/bjtu/cloud/web/TaskController.java
+++ src/main/java/com/bjtu/cloud/web/TaskController.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -83,10 +90,43 @@ public class TaskController {
 
   //创建任务
   @RequestMapping(value = "api/task/create", method = RequestMethod.POST)
-  public RestResult<TaskInfo> create(String nodeId, String hostPath, Integer type,
-                                     Integer mode, Integer times, String startTime, HttpSession session) {
+  public RestResult<TaskInfo> create(String nodeId, Integer type,
+                                     Integer mode, Integer times, String startTime, HttpSession session, HttpServletRequest request) throws IllegalStateException, IOException{
     try {
       String operatorName = session.getAttribute("userName").toString();
+      String path = "/Users/Kafukaaa/Downloads/file/" + operatorName + "/" + nodeId;
+      String hostPath = null;
+      File f = null;
+      //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+      CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+          request.getSession().getServletContext());
+      //检查form中是否有enctype="multipart/form-data"
+      if(multipartResolver.isMultipart(request))
+      {
+        //将request变成多部分request
+        MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+        //获取multiRequest 中所有的文件名
+        Iterator it= multiRequest.getFileNames();
+        while(it.hasNext())
+        {
+          //一次遍历所有文件
+          MultipartFile file = multiRequest.getFile(it.next().toString());
+          if(file!=null)
+          {
+            f = new File(path,file.getOriginalFilename());
+            hostPath = path + "/" + file.getOriginalFilename();
+            if(!f.exists()) {
+              // 若文件不存在，先创建文件所在的目录
+              f.getParentFile().mkdirs();
+            }
+            //上传
+            file.transferTo(f);
+          }
+
+        }
+
+      }
+
       TaskInfo taskInfo = taskService.createTask(nodeId, hostPath, type, mode, times, startTime, operatorName);
       return RestResult.succ().data(taskInfo).build();
     }catch (Exception e){
@@ -105,6 +145,51 @@ public class TaskController {
       e.printStackTrace();
       return RestResult.fail().msg(e.toString()).build();
     }
+  }
+
+  /*
+     *采用spring提供的上传文件的方法
+     */
+  @RequestMapping( value = "SpringMVC006/springUpload")
+  public String  springUpload(HttpServletRequest request) throws IllegalStateException, IOException
+  {
+    //设置服务器放文件的目录
+    String path="/Users/Kafukaaa/Downloads/file";
+    File f = null;
+    long  startTime=System.currentTimeMillis();
+    //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+    CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+        request.getSession().getServletContext());
+    //检查form中是否有enctype="multipart/form-data"
+    if(multipartResolver.isMultipart(request))
+    {
+      //将request变成多部分request
+      MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+      //获取multiRequest 中所有的文件名
+      Iterator iter=multiRequest.getFileNames();
+      while(iter.hasNext())
+      {
+        //一次遍历所有文件
+        MultipartFile file=multiRequest.getFile(iter.next().toString());
+        if(file!=null)
+        {
+          f = new File(path,file.getOriginalFilename());
+          if(!f.exists()) {
+            // 若文件不存在，先创建文件所在的目录
+            System.out.println("Make dir!");
+            f.getParentFile().mkdirs();
+          }
+          //上传
+          file.transferTo(f);
+        }
+
+      }
+
+    }
+    long  endTime=System.currentTimeMillis();
+    System.out.println(f.getName());
+    System.out.println("方法三的运行时间："+String.valueOf(endTime-startTime)+"ms");
+    return "/success";
   }
 
 }
